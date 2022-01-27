acfbeef45ea9f9ab76c4e2adbf8d4d7b71a81093
valentin Depatureaux <vdepatureaux@gmail.com>
2022-01-27 16:26:41 +0100

    Partie 3 - Fichier ⇒ AMQP

---
+++ file-injector/src/main/java/fr/lernejo/fileinjector/FileMapper.java
@@ -0,0 +1,20 @@
+package fr.lernejo.fileinjector;
+
+public class FileMapper {
+    public Integer id;
+    public String title;
+    public String thumbnail;
+    public String short_description;
+    public String genre;
+    public String platform;
+    public String publisher;
+    public String developer;
+    public String release_date;
+    public String freetogame_profile_url;
+    public String game_url;
+
+
+    public FileMapper() {
+    }
+
+}
--- file-injector/src/main/java/fr/lernejo/fileinjector/Launcher.java
+++ file-injector/src/main/java/fr/lernejo/fileinjector/Launcher.java
@@ -1,16 +1,29 @@
 package fr.lernejo.fileinjector;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.AbstractApplicationContext;
+
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
 
 @SpringBootApplication
 public class Launcher {
 
     public static void main(String[] args) {
         try (AbstractApplicationContext springContext = new AnnotationConfigApplicationContext(Launcher.class)) {
-
-            System.out.println("Hello after starting Spring");
-        }
-    }
-}
+            if (args.length >= 1) {
+                try {
+                    ObjectMapper mapper = new ObjectMapper();
+                    List<FileMapper> games_map = Arrays.asList(mapper.readValue(Paths.get(args[0]).toFile(), FileMapper[].class));
+                    RabbitTemplate template = springContext.getBean(RabbitTemplate.class);
+                    for (FileMapper game : games_map) {
+                        template.setMessageConverter(new Jackson2JsonMessageConverter());
+                        template.convertAndSend("", "game_info", game); } } catch (Exception ex) { ex.printStackTrace(); }
+            } else {
+                System.out.println("you must provide a JSON file"); }
+        }}}
