d6f0032a734c1e5bf6944ce41e36a0a8f66065be
valentin Depatureaux <vdepatureaux@gmail.com>
2022-01-27 18:02:31 +0100

    test above 80% ?

---
+++ search-api/src/test/java/fr/lernejo/search/api/LauncherTest.java
@@ -0,0 +1,15 @@
+package fr.lernejo.search.api;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LauncherTest {
+
+    @Test
+    void main() {
+        Launcher.main(new String[]{});
+    }
+}
